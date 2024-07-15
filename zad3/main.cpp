e860a46fcd8011cbed5e62f836f6910f26fc7534
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-15 20:54:52 +0200

    ex3 not yet testest, first version

--- zad3/main.cpp
+++ zad3/main.cpp
@@ -6,14 +6,58 @@
 //prawidłową odpowiedź wypisz na ekranie
 
 #include <iostream>
+using namespace std;
 
 int main(){
 
+    //inserting a number
+    int aNumber;
+    cin>>aNumber;
 
+    //create a variable to hold ID number for switch expression
+    int id;
 
-
-
-
-
+    //check if dividable by 3 and 5
+    if (aNumber % 3 && aNumber % 5)
+    {
+        id = 1;
+    }
+    //check if dividable by 3 only
+    else if (aNumber % 3 && aNumber % 5 !=0)
+    {
+        id = 2;
+    }
+    //check if dividable by 5 only
+    else if (aNumber % 5 && aNumber % 3 !=0)
+    {
+        id = 3;
+    }
+    //check if it's not dividable by neither 3 nor 5
+    else if (aNumber % 3 != 0 && aNumber%5 != 0)
+    {
+        id = 4;
+    }
+    
+    switch (id)
+    {
+    case 1:
+        cout<<"Your number is dividable by both 3 and 5."<<endl;
+        break;
+    case 2:
+        cout<<"Your number is dividable by 3 only."<<endl;
+        break;
+    case 3:
+        cout<<"Your number is dividable by 5 only."<<endl;
+        break;
+    case 4:
+        cout<<"Your number is not dividable by neither 3 nor 5."<<endl;
+        break;
+    
+    default:
+        cout<<"Error!"<<endl;
+        break;
+    }
+    
+    
     return 0;
 }
