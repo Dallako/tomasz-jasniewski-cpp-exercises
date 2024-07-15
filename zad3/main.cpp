d7a30a324b880e1bb6acd06ce4e906d9154a6079
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-15 20:30:03 +0200

    created directory and template for exercise nr 3

---
+++ zad3/main.cpp
@@ -0,0 +1,19 @@
+//pobierz liczbę całkiwitą z klatiarury, sprawdź czy jest podzielna przez:
+//1.) zarówno 3 jak i 5
+//2.) przez 3, ale nie przez 5
+//3.) przez 5, ale nie przez 3
+//4.) ani nie przez 3, ani nie przez 5
+//prawidłową odpowiedź wypisz na ekranie
+
+#include <iostream>
+
+int main(){
+
+
+
+
+
+
+
+    return 0;
+}
