bb47b6805ce3dbd4aaea1b6ebd63c1a75c7474e2
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-15 20:07:12 +0200

    not sure where is the error, random numbers from memory are transfered to output instead of true or false answer

--- zad2/main.cpp
+++ zad2/main.cpp
@@ -1,10 +1,21 @@
+//1 integer, check if even
+
 #include <iostream>
 using namespace std;
 
 int main(){
+    int liczba;
+    cin>>liczba;
+    bool isEven;
+    if (liczba % 2)
+    {
+        isEven == true;
+    }
+    else isEven == false;
+    
 
-
-
+// test / debug
+cout<<isEven;
 
 return 0;
 };
