18c57cc352b2038b520dff89a8fa5ff7e7db91c6
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-15 20:24:59 +0200

    after long time of debugging i found out i used double equals symbol instead of a single one. exercise nr 2 finished

--- zad2/main.cpp
+++ zad2/main.cpp
@@ -4,18 +4,23 @@
 using namespace std;
 
 int main(){
-    int liczba;
-    cin>>liczba;
-    bool isEven;
-    if (liczba % 2)
-    {
-        isEven == true;
-    }
-    else isEven == false;
-    
 
-// test / debug
-cout<<isEven;
+int yourNumber;
+cout<<"Please insert your number: ";
+cin>>yourNumber;
+bool isNumberEven = false;
+if (yourNumber % 2)
+{
+    isNumberEven = true;
+}
+else isNumberEven = false;
+
+if (!isNumberEven)
+{
+    cout<<"Your number is even";
+}
+else cout<<"Your number is NOT even";
+
 
 return 0;
 };
