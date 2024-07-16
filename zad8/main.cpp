23e62b87d040ec17450636122f4f232556265ec2
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-16 20:50:36 +0200

    ex8 template

---
+++ zad8/main.cpp
@@ -0,0 +1,14 @@
+//Pobierz z klawiatury dwie liczby float i znak działania (jeden z tych +, -, *, /).
+//W zależności od znaku wykonaj na pobranych dwóch liczbach odpowiednie działanie.
+//Zwróć uwagę na dzielenie przez 0!
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+
+
+    return 0;
+}
