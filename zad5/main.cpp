e0d3d8bf6784d7c38e468143362e19e17c10e8cc
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-16 19:13:26 +0200

    template file for ex5

---
+++ zad5/main.cpp
@@ -0,0 +1,16 @@
+//masz takie wyrażenie: (((a1+a2)*a3)-a4)/a5 (elementy od a1 do a5 są typu float).
+//Pobierz z klawiatury każdą ze zmiennych a1 do a5, oblicz wartość wyrażenia i wyświetl wynik.
+//w przypadku dzielenia przez 0 informacja o błędzie i nie wykonywanie się programu.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+
+
+
+
+    return 0;
+}
