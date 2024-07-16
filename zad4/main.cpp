7166248fa2da9bfa966c64ef485ab0f52ad7b66f
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-16 17:10:13 +0200

    prepared template file for ex4

---
+++ zad4/main.cpp
@@ -0,0 +1,13 @@
+//pobierz znak (char) z klawiatury. sprawdź czy to spółgłoska, samogłoska czy cyfra.
+//poinformuj jaki to znak, uwzględnij tylko małe litery angielskiego alfabetu i cyfry.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+
+
+
+    return 0;
+}
