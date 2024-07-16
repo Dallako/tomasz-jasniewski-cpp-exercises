80f7a935a79154d8a425cb87fe1a24a247bc735e
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-16 19:34:27 +0200

    this was honestly too easy

--- zad5/main.cpp
+++ zad5/main.cpp
@@ -6,7 +6,30 @@
 using namespace std;
 
 int main(){
+    //create variables and take input from user
+    int a1, a2, a3, a4, a5;
+    cout<<"Input first value: ";
+    cin>>a1;
+    cout<<"Input second value: ";
+    cin>>a2;
+    cout<<"Input third value: ";
+    cin>>a3;
+    cout<<"Input fourth value: ";
+    cin>>a4;
+    cout<<"Input fifth value: ";
+    cin>>a5;
 
+    //do math and edge case handling
+    int result;
+    if (a5 == 0)
+    {
+        cout<<"Dividing by 0 detected. Deleting your operating system rn."<<endl;
+        return 1;
+    }
+    else result = (((a1+a2)*a3)-a4)/a5;
+
+    //print the results
+    cout<<"Your result is: "<<result<<endl;
 
 
 
