e14325212d8902f21087b95be5f85738e14d9951
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-16 21:09:41 +0200

    done ex8

--- zad8/main.cpp
+++ zad8/main.cpp
@@ -5,10 +5,57 @@
 #include <iostream>
 using namespace std;
 
+//function checking if you're not dividing by 0
+bool edgeCaseHandling(int n, char k)
+{
+    if (n == 0 && k == '/')
+    {
+        return true;
+    }
+    else return false;
+}
+
 int main(){
+    //create variables and take input
+    float a, b;
+    char x;
+    cout<<"Please input first number: ";
+    cin>>a;
+    cout<<"\nPlease input second number: ";
+    cin>>b;
+    cout<<"\nPlease input arithmetic operator: ";
+    cin>>x;
 
+    //Edge Case Handling
+    if (edgeCaseHandling(b, x))
+    {
+        cout<<"Detected dividing by 0!!!"<<endl;
+        return 1;
+    }
+    
+    //do math
+    float result;
+    
+    switch (x)
+    {
+    case '+':
+        result = a+b;
+        break;
+    case '-':
+        result = a-b;
+        break;
+    case '*':
+        result = a*b;
+        break;
+    case '/':
+        result = a/b;
+        break;
+    default:
+        cout<<"Unidentified switch error";
+        return 1;
+        break;
+    }
 
-
-
-    return 0;
+    cout<<result;
+return 0;
 }
