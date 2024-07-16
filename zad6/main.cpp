17f3faeaf6d0609d5704e22b0e0c65e56390b972
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-16 19:42:16 +0200

    ex6 template file

---
+++ zad6/main.cpp
@@ -0,0 +1,13 @@
+//pobierz 5 liczb z klawiatury i wyświetl informacje ile z nich było parzystych, a ile nieparzystych
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+
+
+
+    return 0;
+}
