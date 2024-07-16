f204aa690b670274aec882b754ae6257cd61d4f2
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-16 17:28:29 +0200

    attempt 1: using arrays and comparing char variable to them

--- zad4/main.cpp
+++ zad4/main.cpp
@@ -2,11 +2,20 @@
 //poinformuj jaki to znak, uwzględnij tylko małe litery angielskiego alfabetu i cyfry.
 
 #include <iostream>
+#include <array>
 using namespace std;
 
 int main(){
-    
+    //creating variable and asking user for input
+    char character;
+    cin>>character;
 
+    //create array with consonants
+    array<char, 21> consonants = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'};
+    //create array with vowels
+    array<char, 5> vowels = {'a', 'e', 'i', 'o', 'u'};
+    //create array with digits
+    array<char, 10> digits = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 
     return 0;
