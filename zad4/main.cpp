c05ef105440cde1030b01773c3eec767359e7b71
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-16 18:12:35 +0200

    attempt failed; going to try with tables instead of arrays

--- zad4/main.cpp
+++ zad4/main.cpp
@@ -3,11 +3,13 @@
 
 #include <iostream>
 #include <array>
+#include <algorithm>
 using namespace std;
 
 int main(){
     //creating variable and asking user for input
     char character;
+    cout<<"Please insert your character: ";
     cin>>character;
 
     //create array with consonants
@@ -17,6 +19,32 @@ int main(){
     //create array with digits
     array<char, 10> digits = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+    //compare character to arrays
+    
+        find (digits.begin(), digits.end(), character)
+            ? cout<<"Your character is NOT a digit.\n"  //says this if condition==true
+            : cout<<"Your character is a digit.\n";     //says this if condition==false
+        find (vowels.begin(), vowels.end(), character)
+            ? cout<<"Your character is NOT a vowel.\n"
+            : cout<<"Your character is a vowel.\n";
+        find (consonants.begin(), consonants.end(), character)
+            ? cout<<"Your character is NOT a consonant.\n"
+            : cout<<"Your character is a consonant.\n";
+        cout<<character;
+    
 
+
+
+/* example find() function from documentation:
+
+int main()
+{
+    const auto v = {1, 2, 3, 4};
+ 
+    for (const int n : {3, 5})
+        (std::find(v.begin(), v.end(), n) == std::end(v))
+            ? std::cout << "v does not contain " << n << '\n'
+            : std::cout << "v contains " << n << '\n';
+*/
     return 0;
 }
