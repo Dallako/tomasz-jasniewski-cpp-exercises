d2d5561348152d48d72ab4bd6aa834e8b1434645
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-20 20:42:53 +0200

    ex10 template

---
+++ zad10/main.cpp
@@ -0,0 +1,12 @@
+//Wyświetl liczby całkowite od 1 do 100. Spróbuj zrobić to wykorzystując różne pętle (for, while, do...while).
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+
+
+    return 0;
+}
