b2d6d7de700adbabdee345f8b05dda0d9e3a4539
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-20 20:30:19 +0200

    template for exercise 9

--- zad9/main.cpp
+++ zad9/main.cpp
@@ -1,4 +1,4 @@
-//instructions
+//Pobierz liczbę całkowitą z klawiatury, i wykonaj na niej poniższe operacje: jeśli liczba była ujemna, zmniejsz ją o 1. Jeżeli była dodatnia, zwiększ ją o 1. Jeżeli liczba była równa zeru, pozostaw bez zmian; Wyświetl liczbę po zmianach. Następnie określ parzystość liczby po zmianach i wyświetl informacje na ten temat (tak/nie).
 
 #include <iostream>
 using namespace std;
