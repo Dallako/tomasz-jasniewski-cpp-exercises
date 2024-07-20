3c1a26721eca55993012a0fad4e81a18d25da6cf
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-20 20:39:54 +0200

    finished, easy

--- zad9/main.cpp
+++ zad9/main.cpp
@@ -4,8 +4,29 @@
 using namespace std;
 
 int main(){
+    //creating a variable and take input form user
+    int number;
+    cout<<"Please input your number: ";
+    cin>>number;
 
+    if (number<0)
+    {
+        number--;
+    }
+    else if (number>0)
+    {
+        number++;
+    }
 
+    cout<<"Your number after changes: "<<number<<endl;
+
+    if (number%2==0)
+    {
+        cout<<"Your number "<<number<<" is even";
+    }
+    else cout<<"Your number "<<number<<" is not even";
+    
+    
 
 
     return 0;
