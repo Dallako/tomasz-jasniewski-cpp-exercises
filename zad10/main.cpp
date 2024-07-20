576c70fb222f08edcb7577804ff7cd4090ef933a
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-20 20:51:09 +0200

    ex10 finished, ez

--- zad10/main.cpp
+++ zad10/main.cpp
@@ -4,8 +4,28 @@
 using namespace std;
 
 int main(){
-
-
+    cout<<"\"for\" loop:"<<endl;
+    for (int i = 1; i <= 100; i++)
+    {
+        cout<<i<<endl;
+    }
+    
+    int iterator = 1;
+    cout<<"\"while\" loop:"<<endl;
+    while (iterator<=100)
+    {
+       cout<<iterator<<endl;
+       iterator++;
+    }
+    
+    iterator = 1;
+    cout<<"\"do...while\" loop:"<<endl;
+    do
+    {
+        cout<<iterator<<endl;
+       iterator++;
+    } while (iterator<=100);
+    
 
 
     return 0;
