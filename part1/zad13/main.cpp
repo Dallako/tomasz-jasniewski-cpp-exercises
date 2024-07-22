b090cec6662b9e8beea7185dfabdb9d6ece224a0
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-22 18:08:49 +0200

    ex13 completed

--- part1/zad13/main.cpp
+++ part1/zad13/main.cpp
@@ -4,9 +4,32 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    
+//is dividable by both 11 and 5
+bool dividableBy11And5(int n)
+{
+    if (n%11==0 && n%5==0)
+    {
+        return true;
+    }
+    else return false;
+}
 
+int main(){
+    short skipped=0;
+    short displayed=0;
+
+    for (short i = 1; i <= 120; i++)
+    {
+        if (!dividableBy11And5(i))
+        {
+            cout<<i<<endl;
+            displayed++;
+        }
+        else skipped++;
+    }
+    
+    cout<<"\nNumber of displayed integers: "<<displayed<<endl;
+    cout<<"\nNumber of skipped integers: "<<skipped<<endl;
 
 
 return 0;
