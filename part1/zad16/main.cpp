3a64ebe8e3fa1c730abaa2033cf4a5b11c38c681
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-22 21:04:03 +0200

    NOT FINISHED, tomorrow rethink tf ur doing

--- part1/zad16/main.cpp
+++ part1/zad16/main.cpp
@@ -7,9 +7,18 @@
 using namespace std;
 
 int main(){
+    //create an array
+    array<size_t, 100> theSequence = {100, 99, 97, 94, 90, 85}; //0-5
 
+    //here put the solution
+    
 
+    //print out array
+    for_each(theSequence.begin(), theSequence.end(), [] (size_t x)
+        {
+            cout<<x<<' ';
+        }
+    );
 
-
-    return 0;
+return 0;
 }
