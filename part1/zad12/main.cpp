88da6ccdcfa4602fb380dc2b3ae5d44732c014e9
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-22 17:54:10 +0200

    ex12 completed + ex13 template

--- part1/zad12/main.cpp
+++ part1/zad12/main.cpp
@@ -3,10 +3,24 @@
 #include <iostream>
 using namespace std;
 
+bool isZero(int n)
+{
+    if (n==0)
+    {
+        return true;
+    }
+    else return false;
+    
+}
 int main(){
-
-
-
-
+    for (int i = -25; i <= 25; i++)
+    {
+        if (!isZero(i))
+        {
+            cout<<i<<endl;
+        }
+        else cout<<""; //same thing as in exercise 11
+    }
+    
     return 0;
 }
---
+++ part1/zad13/main.cpp
@@ -0,0 +1,13 @@
+//Display integers from 1 to 120, excluding numbers dividable by both 11 and 5.
+//Display information how many numbers was displayed, and how many was skipped.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+
+
+
+return 0;
+}
