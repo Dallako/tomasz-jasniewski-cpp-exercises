39cd4ee45523fbd849f1143fa2d2f5698273ec0b
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-22 20:22:31 +0200

    i belive i used 100% of my brain power and it's still not ideal

--- part1/zad15/main.cpp
+++ part1/zad15/main.cpp
@@ -11,9 +11,28 @@ int main(){
     //create an array
     array<size_t, 100> sequence = {1,2,2,3,3,3,4,4,4,4}; //0-9
 
+    //do it more manually since i can't find a solution to automate the entirelity of this
+    int nextNumber = 5;
+    int nextIndex = 10;
+    int oldIndex = 10;
+    while (nextIndex<100)
+    {
+        sequence.at(nextIndex) = nextNumber;
+        nextIndex++;
+        if (nextIndex==oldIndex+nextNumber)
+        {
+            oldIndex=nextIndex;
+            nextNumber++;
+        }
+    }
     
-    
-    
+    //display an array
+    cout<<"A full sequence would be: \n";
+    for_each(sequence.begin(), sequence.end(), [](size_t x)
+        {
+            cout<<x<<' ';
+        }
+    );
 
 return 0;
 }
