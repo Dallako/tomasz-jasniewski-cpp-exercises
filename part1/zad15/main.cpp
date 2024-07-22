e54ff6bb952e0a709252bbcd2d7751a50fe6ec54
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-22 19:19:12 +0200

    ex15 template

---
+++ part1/zad15/main.cpp
@@ -0,0 +1,15 @@
+//Guess the pattern of a sequence and then display its 100 elements.
+//Start of sequence: [1,2,2,3,3,3,4,4,4,4]
+
+#include <iostream>
+#include <array>
+#include <algorithm>    //to use "for_each()"
+
+using namespace std;
+
+int main(){
+    
+    
+
+    return 0;
+}
