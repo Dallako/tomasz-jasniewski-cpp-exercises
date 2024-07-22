256d9f26175f3dc85e98803638632b85e88ca02d
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-22 20:26:43 +0200

    ex16 template

---
+++ part1/zad16/main.cpp
@@ -0,0 +1,15 @@
+//Guess the pattern of a sequence and then display its 100 elements.
+//Start of sequence: [100, 99, 97, 94, 90, 85]
+
+#include <iostream>
+#include <array>
+#include <algorithm>    //to use "for_each()" to print out full array
+using namespace std;
+
+int main(){
+
+
+
+
+    return 0;
+}
