6d787e79106a56bb656e70f6cc3bfa60b0466ec0
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-22 19:54:17 +0200

    step 1: create an array

--- part1/zad15/main.cpp
+++ part1/zad15/main.cpp
@@ -3,13 +3,17 @@
 
 #include <iostream>
 #include <array>
-#include <algorithm>    //to use "for_each()"
+#include <algorithm>    //to use "for_each()" to print out full array
 
 using namespace std;
 
 int main(){
+    //create an array
+    array<size_t, 100> sequence = {1,2,2,3,3,3,4,4,4,4}; //0-9
+
+    
     
     
 
-    return 0;
+return 0;
 }
