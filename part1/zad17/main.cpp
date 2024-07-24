ec7e366b8392ea6dad0ed8e1beaeac62ae171726
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-07-24 20:04:09 +0200

    ex17 template

---
+++ part1/zad17/main.cpp
@@ -0,0 +1,13 @@
+//Guess the pattern of a sequence and then display its 100 elements.
+//Start of sequence: [6,2,8,3,10,4,12,5,14,6]
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+
+
+    return 0;
+}
