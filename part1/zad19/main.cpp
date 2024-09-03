051f1d2b34a78aef2bcc99cab2697debbca4de74
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-09-03 19:27:27 +0200

    why was it so easy when ex14-17 were melting my brain?

--- part1/zad19/main.cpp
+++ part1/zad19/main.cpp
@@ -4,7 +4,15 @@
 using namespace std;
 
 int main(){
-
+    for (int i = 1; i <= 1000; i++)
+    {
+        if (i % 6 == 0)
+        {
+            cout<<i<<" ";
+        }
+        
+    }
+    
 
 
     return 0;
