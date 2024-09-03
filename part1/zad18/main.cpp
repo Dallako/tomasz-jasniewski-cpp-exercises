cfd69b033ed9b260db0c02f553a5d25935faccbd
Dallak Sidorov <dawidsz.1@hotmail.com>
2024-09-03 19:20:44 +0200

    well i did the thing, i used goto

--- part1/zad18/main.cpp
+++ part1/zad18/main.cpp
@@ -4,7 +4,21 @@
 #include <iostream>
 
 int main(){
+    //take input from user
+    label:;
+    int a = 0;
+    std::cout<<"Give me an integer: ";
+    std::cin>>a;
 
+    //double it
+    int b = a*2;
 
+    //check if 'b' between 1 and 10
+    if (a>=1 && a<=10)
+    {
+        return 0;
+    }
+    else goto label;
+    
     return 0;
 }
